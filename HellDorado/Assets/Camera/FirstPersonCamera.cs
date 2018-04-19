dda443c6f36cff27934dc5288f6ced9e29da94d0
carkhem <38533272+carkhem@users.noreply.github.com>
2018-04-19 18:09:29 +0200

    Fireball
    It is now possible to cast fireball

--- HellDorado/Assets/Camera/FirstPersonCamera.cs
+++ HellDorado/Assets/Camera/FirstPersonCamera.cs
@@ -17,6 +17,10 @@ public class FirstPersonCamera : MonoBehaviour
     public float verticalSens = 10.0f;
     public float _rotationX = 0;
 
+
+    public bool lockCursor = true;
+    private bool m_cursorIsLocked = true;
+
     private void Update()
     {
         if(axes == RotationAxis.MouseX)
@@ -29,6 +33,50 @@ public class FirstPersonCamera : MonoBehaviour
 
             float rotationY = transform.localEulerAngles.y;
             transform.localEulerAngles = new Vector3(_rotationX, rotationY, 0);
+        }
+        UpdateCursorLock();
+    }
+
+
+    // lock försök nedan
+    /*
+    public void SetCursorLock(bool value)
+    {
+        lockCursor = value;
+        if (!lockCursor)
+        {//we force unlock the cursor if the user disable the cursor locking helper
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+        }
+    } */
+
+    public void UpdateCursorLock()
+    {
+        //if the user set "lockCursor" we check & properly lock the cursos
+        if (lockCursor)
+            InternalLockUpdate();
+    }
+
+    private void InternalLockUpdate()
+    {
+        if (Input.GetKeyUp(KeyCode.Escape))
+        {
+            m_cursorIsLocked = false;
+        }
+        else if (Input.GetMouseButtonUp(0))
+        {
+            m_cursorIsLocked = true;
+        }
+
+        if (m_cursorIsLocked)
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
+        }
+        else if (!m_cursorIsLocked)
+        {
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
         }
     }
 }
