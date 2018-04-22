d64e6626b89ac97150b17b39299df66344274c4e
Aulbn <38533964+Aulbn@users.noreply.github.com>
2018-04-22 11:57:02 +0200

    Fireball 2.0
    Hur jag tycker Fireball-scriptet borde se ut. Hur man skjuter iväg klotet borde skötas av player-scriptet eller t.ex. Grounded-statet.

---
+++ HellDorado/Assets/Fireball/Fireball2.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fireball2 : MonoBehaviour {
+
+	public float lifetime = 2;
+	public float speed = 2;
+	public float maxDamage = 2;
+	public float minDamage = 1;
+	public GameObject explosionPrefab;
+
+	private float damage;
+	private float timer;
+
+	void Start () {
+		damage = maxDamage; //Behöver kanske inte den här raden.
+		timer = 0;
+	}
+	
+	void Update () {
+		//Åk framåt i den hastighet vi vill.
+		transform.Translate (Vector3.forward * speed * Time.deltaTime);
+
+		//Minska damage över tid
+		timer += Time.deltaTime;
+		damage = Mathf.Lerp (maxDamage, minDamage, timer / lifetime);
+
+		//Förstör när livstiden är nådd.
+		if (timer >= lifetime) {
+			Explode ();
+		}
+	}
+
+	private void Explode(){
+		GameObject explosion = Instantiate (explosionPrefab, transform.position, transform.rotation);
+		//explosion.particleSystem.particleEmitter -- Sätt storleken på explosionen på nåt sätt.
+		GameObject.Destroy (gameObject);
+	}
+
+	void OnCollisionEnter(){
+		Explode ();
+	}
+
+	public float GetDamage(){
+		return damage;
+	}
+
+}
