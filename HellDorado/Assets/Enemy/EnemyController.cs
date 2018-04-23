eb468ab7f52004e6f918f7ff6d943310dbfc2c18
Aulbn <38533964+Aulbn@users.noreply.github.com>
2018-04-23 20:55:18 +0200

    Enemy FOV
    - Fienden kan upptäcka spelaren
    - Nya states

--- HellDorado/Assets/Enemy/EnemyController.cs
+++ HellDorado/Assets/Enemy/EnemyController.cs
@@ -7,4 +7,34 @@ public class EnemyController : Controller {
 	[Header("Movement")]
 	public float runSpeed;
 	public float walkSpeed;
+	[Header("Stats")]
+	public float damage;
+	public float health;
+	[Header("Sight")]
+	public float fov;
+	public float viewDistance;
+	public float detectionTime;
+	public Transform player;
+	private Vector3 playerDirection;
+
+	void Update(){
+		if (health <= 0) {
+			TransitionTo<DeadState> ();
+		}
+
+		playerDirection = player.position - transform.position;
+		RaycastHit hit;
+		if(Physics.Raycast(transform.position, playerDirection, out hit, viewDistance)){
+			if (hit.transform.CompareTag("Player")){
+				if (Vector3.Angle (transform.forward, playerDirection) < fov) {
+					//Player in field of view
+					Debug.DrawRay (transform.position, playerDirection, Color.green);
+				} else {
+					Debug.DrawRay (transform.position, playerDirection, Color.red);
+				}
+			}
+		}
+		Debug.DrawRay (transform.position, Quaternion.AngleAxis(fov, transform.up) * transform.forward * viewDistance);
+		Debug.DrawRay (transform.position, Quaternion.AngleAxis(-fov, transform.up) * transform.forward * viewDistance);
+	}
 }
---
+++ HellDorado/Assets/Enemy/States/Scripts/CombatState.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Enemy/States/Combat")]
+public class CombatState : State {
+	public float hitRange;
+
+
+	public override void Update (){
+		
+	}
+}
