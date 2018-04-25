dc8e8b85685246ca65749c985a44b6cdda347879
Aulbn <38533964+Aulbn@users.noreply.github.com>
2018-04-25 18:31:09 +0200

    Dialogsystem
    - Dialogsystem
    - Trigger-script. Sätte det på en trigger och trigga metoder från vilket script ni vill

---
+++ HellDorado/Assets/Scripts/DialogueSystem.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogueSystem : MonoBehaviour {
+	public GameObject dialogueBox;
+	public Text uiText;
+	public Text uiName;
+	public string speakerName;
+	public string[] pages;
+	private int currentPage = 0;
+
+	void Awake(){
+		dialogueBox.SetActive (false);
+	}
+
+	public void StartDialogue(){
+		dialogueBox.SetActive (true);
+		currentPage = 0;
+		uiText.text = pages [currentPage];
+		uiName.text = speakerName;
+	}
+
+	public void Update(){
+		if (Input.GetKeyDown (KeyCode.E) && dialogueBox.activeSelf) {
+			if (pages.Length - 1 > currentPage) {
+				print ("NextPage");
+				currentPage += 1;
+				uiText.text = pages [currentPage];
+			} else {
+				dialogueBox.SetActive (false);
+			}
+		}
+	}
+}
---
+++ HellDorado/Assets/Scripts/Trigger.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Trigger : MonoBehaviour {
+	public string[] interactableTags = {"Player"};
+	public UnityEvent TriggerEnter;
+	public UnityEvent TriggerExit;
+
+	void OnTriggerEnter(Collider col){
+		foreach (string t in interactableTags) {
+			if (col.transform.CompareTag (t)) {
+				TriggerEnter.Invoke ();
+				break;
+			}
+		}
+	}
+
+	void OnTriggerExit(Collider col){
+		foreach (string t in interactableTags) {
+			if (col.transform.CompareTag (t)) {
+				TriggerExit.Invoke ();
+				break;
+			}
+		}
+	}
+}
