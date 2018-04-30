72a79c90d6e1e559096b18ecadfc45037db943ef
carkhem <38533272+carkhem@users.noreply.github.com>
2018-04-30 12:29:00 +0200

    Revert "Revert "CheckPoint & RewindObjects""
    This reverts commit 9aba9637699c31c6567759afdb9f4d43864e6e51.

---
+++ HellDorado/Assets/Scripts/CheckPoint.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckPoint : MonoBehaviour {
+
+	public bool activated = false;
+	public static GameObject[] checkPoints;
+
+	public static Vector3 GetActiveCheckPointPosition ()
+	{
+
+		//Om det inte finns någon checkpoint aktiverad så är positionen "default"
+		Vector3 position = new Vector3 (0, 0, 0);
+
+		if (checkPoints != null) {
+			foreach (GameObject checkpoint in checkPoints) {
+				if (checkpoint.GetComponent<CheckPoint> ().activated) {
+					position = checkpoint.transform.position;
+					break;
+				}
+			}
+		}
+
+		return position;
+	}
+
+	private void ActivateCheckPoint ()
+	{
+		//går igenom alla checkpoints i listan och deaktiverar dem
+		foreach (GameObject cp in checkPoints) {
+			cp.GetComponent<CheckPoint> ().activated = false;
+		}
+
+		//aktiverar nuvarande checkpoint
+		activated = true;
+	}
+
+	void Start ()
+	{
+		checkPoints = GameObject.FindGameObjectsWithTag ("CheckPoint");
+	}
+
+	void OnTriggerEnter (Collider other)
+	{
+
+		if (other.tag == "Player") {
+			ActivateCheckPoint ();
+		}
+	}
+}
---
+++ HellDorado/Assets/Scripts/InteractObject.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InteractObject : MonoBehaviour
+{
+
+    public Text interactText;
+    public float rayRange;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastObject();
+    }
+
+    public void RaycastObject()
+    {
+
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit, rayRange) && (hit.collider.gameObject.tag == "Lever"))
+        {
+            GameObject Lever = hit.collider.gameObject;
+            interactText.text = "Press [F] to interact with " + hit.collider.gameObject.tag;
+            if (Input.GetKeyDown(KeyCode.F))
+                Lever.GetComponent<GateLever>().open = !Lever.GetComponent<GateLever>().open;
+        }
+        else
+        {
+            interactText.text = "";
+        }
+    }
+}
