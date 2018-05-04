fced00a80c7de6d3217617a8af16dd109b313132
Albin <38533964+Aulbn@users.noreply.github.com>
2018-05-04 23:54:05 +0200

    gfxnth

--- HellDorado/Assets/Enemy/States/Scripts/CombatState.cs
+++ HellDorado/Assets/Enemy/States/Scripts/CombatState.cs
@@ -12,6 +12,7 @@ public class CombatState : State {
 	private Vector3 lastKnownPos;
 	public float logicFollowTime = 2f;
 	private float logicFollowTimer = 0;
+	private float rotTimer = 0;
 	[Header("Fighting")]
 	public MinMaxFloat attackWait;
 	private float currentAttackWait;
@@ -30,20 +31,26 @@ public class CombatState : State {
 		Debug.Log (transform.name + ": " + _controller.CurrentState.name);
 		transform.GetComponent<NavMeshAgent> ().enabled = true;
 		attackTimer = 0;
+		rotTimer = 0;
 		currentAttackWait = Random.Range (attackWait.Min, attackWait.Max);
 		agent.speed = runSpeed;
 		logicFollowTimer = 0;
-//		transform.LookAt (_controller.player);
 		lastKnownPos = _controller.player.position;
 	}
 
 	public override void Update (){
+		//Det första som händer är att den tittar mot spelarens sista position
+		if (rotTimer < 1) {
+			rotTimer += Time.deltaTime;
+		}
+		transform.rotation = Quaternion.Lerp (transform.rotation, Quaternion.LookRotation(lastKnownPos - transform.position), Mathf.Abs(rotTimer / 1));
+
 		//Stannar för skarpt. Vill egentligen hitta en vector mellan tarnsform och player som är stopDistance ifrån playern.
 		if (_controller.InSight (_controller.player)) {
 			lastKnownPos = _controller.player.position;
 		}
 
-		if (!_controller.IsMoving(agent) && !_controller.InSight(_controller.player)) {
+		if (!_controller.IsMoving(agent) && !_controller.InSight(_controller.player) && rotTimer >= 1) {
 			_controller.TransitionTo<SearchingState> ();
 		}
 
@@ -68,12 +75,6 @@ public class CombatState : State {
 				_controller.TransitionTo<AttackState> ();
 			}
 		}
-
-		//Vill ha en mer smooth LookAt
-//		transform.LookAt (new Vector3 (_controller.player.position.x, transform.position.y, _controller.player.position.z));
-//		transform.rotation = Quaternion.LookRotation(_controller.player.position - transform.position);
-
-		transform.rotation = Quaternion.Lerp (transform.rotation, Quaternion.LookRotation(_controller.player.position - transform.position), Time.deltaTime * 10);
 	}
 
 	public override void Exit (){
