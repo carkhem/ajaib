5f108687ac0e16f08aefc0a84a2239c241be426e
Aulbn <38533964+Aulbn@users.noreply.github.com>
2018-05-08 00:29:39 +0200

    cool shit

---
+++ HellDorado/Assets/Animations/AnimationRecieveStone.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+public class AnimationRecieveStone : MonoBehaviour {
+
+	public PlayableDirector director;
+	private Vector3 startPosition = new Vector3 (-11.93f, 1.51f, 3.69f);
+	private GameObject player;
+
+	void Start (){
+		player = PlayerStats.instance.transform.gameObject;
+	}
+
+	public void StartAnimation(){
+		GetComponent<FPSCamera> ().enabled = false;
+		director.Play ();
+	}
+
+	void Update(){
+		if (director.time > 0) {
+			player.transform.position = Vector3.Lerp (player.transform.position, startPosition, Time.deltaTime * 50);
+		}
+		if (director.time >= director.duration) {
+			GetComponent<FPSCamera> ().enabled = true;
+		}
+	}
+}
