db4c941c32247f42e5992f441a7175134f198756
Aulbn <38533964+Aulbn@users.noreply.github.com>
2018-05-07 13:04:39 +0200

    grejer

--- HellDorado/Assets/Abilities/Fireball/Fireball2.cs
+++ HellDorado/Assets/Abilities/Fireball/Fireball2.cs
@@ -35,6 +35,7 @@ public class Fireball2 : MonoBehaviour {
 
 		//Sluta emitta när livstiden är nådd.
 		if (timer >= lifetime) {
+			GetComponent<BoxCollider> ().enabled = false;
 			StopEmitting();
 		}
 		DestroyIfDead ();
---
+++ HellDorado/Assets/Scripts/AnimatedKey.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+public class AnimatedKey : MonoBehaviour {
+
+	public PlayableDirector director;
+	public Transform hand;
+
+	public void Update(){
+		if (director == null)
+			return;
+		
+		if (director.time >= 10.58 && director.time <= 12) {
+			transform.SetParent (hand);
+		}
+//		if (director.time >= 8 &&  director.time <= 9) {
+//			transform.SetParent (hand);
+//		}
+	}
+
+}
