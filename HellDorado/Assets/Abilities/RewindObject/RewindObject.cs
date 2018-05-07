e05fe2ddee497abf26339da1df4a5933a3b43f27
Aulbn <38533964+Aulbn@users.noreply.github.com>
2018-05-07 17:47:15 +0200

    aj då

--- HellDorado/Assets/Abilities/RewindObject/RewindObject.cs
+++ HellDorado/Assets/Abilities/RewindObject/RewindObject.cs
@@ -5,23 +5,30 @@ using UnityEngine;
 public class RewindObject : MonoBehaviour {
 
 	private GameObject activeGameobject;
+	private bool inSight = false;
+	public Sprite icon;
+
+	void Update(){
+		UpdateFeedback ();
+	}
 
 	public void UseRewindObject(){
 		Ray ray = Camera.main.ScreenPointToRay(new Vector3 (Screen.width / 2, Screen.height / 2, 0));
 		RaycastHit hit;
 
-//		if ((Physics.Raycast (ray, out hit, 50) && (hit.collider.gameObject.tag == "Interactable")) && ) {
-//			if (hit.collider.GetComponent<ObjectTimeBody> () != null) {
-//				UpdateFeedback ();
-//				activeGameobject = hit.collider.gameObject;
-//				hit.collider.gameObject.GetComponent<ObjectTimeBody> ().StartRewind ();
-//			}
-//		}
+		if ((Physics.Raycast (ray, out hit, 50) && (hit.collider.gameObject.tag == "Interactable")) && hit.collider.GetComponent<ObjectTimeBody> () != null) {
+			inSight;
+			activeGameobject = hit.collider.gameObject;
+			hit.collider.gameObject.GetComponent<ObjectTimeBody> ().StartRewind ();
+		}
 	}
 
 	public void UpdateFeedback(){
-		
+		if (inSight) {
+			
+		}
 	}
+
 
 	public GameObject HitInfo(){
 		return activeGameobject;
