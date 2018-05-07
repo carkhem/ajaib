090f3705027a22dd8b6c5889063513b484208da6
Aulbn <38533964+Aulbn@users.noreply.github.com>
2018-05-08 01:01:33 +0200

    senpaiiiiii

--- HellDorado/Assets/Animations/AnimationRecieveStone.cs
+++ HellDorado/Assets/Animations/AnimationRecieveStone.cs
@@ -6,15 +6,22 @@ using UnityEngine.Playables;
 public class AnimationRecieveStone : MonoBehaviour {
 
 	public PlayableDirector director;
-	private Vector3 startPosition = new Vector3 (-11.93f, 1.51f, 3.69f);
+	private Vector3 startPosition = new Vector3 (-11.93f, 1.598708f, 3.69f);
 	private GameObject player;
+	public GameObject gameHands;
+	private FPSCamera fps;
 
 	void Start (){
 		player = PlayerStats.instance.transform.gameObject;
+		director.Stop ();
+		director.playOnAwake = false;
 	}
 
 	public void StartAnimation(){
-		GetComponent<FPSCamera> ().enabled = false;
+		gameHands.SetActive(false);
+		director.transform.gameObject.SetActive (true);
+		fps = GetComponent<FPSCamera> ();
+		fps.enabled = false;
 		director.Play ();
 	}
 
@@ -24,6 +31,10 @@ public class AnimationRecieveStone : MonoBehaviour {
 		}
 		if (director.time >= director.duration) {
 			GetComponent<FPSCamera> ().enabled = true;
+			gameHands.SetActive(true);
+			director.gameObject.SetActive (false);
+			fps.enabled = true;
+			GameManager.instance.ChangePlayerLevel (2);
 		}
 	}
 }
