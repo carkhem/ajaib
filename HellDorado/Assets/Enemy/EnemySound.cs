686786220221de6f16affe860d13a420e757d94b
carkhem <38533272+carkhem@users.noreply.github.com>
2018-05-08 00:30:43 +0200

    EnemySwingSound

---
+++ HellDorado/Assets/Enemy/EnemySound.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySound : MonoBehaviour {
+
+	public AudioClip swingSound;
+	private AudioSource source;
+
+	// Use this for initialization
+	void Start () {
+		source = GetComponent<AudioSource> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void PlaySwingSound(){
+		if (!source.isPlaying) {
+			source.PlayOneShot (swingSound);
+		}
+	}
+}
