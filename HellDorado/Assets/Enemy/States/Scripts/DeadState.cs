e49ad90ff17abb69661ec9bd7e88829493b43dd3
Aulbn <38533964+Aulbn@users.noreply.github.com>
2018-05-07 21:10:27 +0200

    golv hål

--- HellDorado/Assets/Enemy/States/Scripts/DeadState.cs
+++ HellDorado/Assets/Enemy/States/Scripts/DeadState.cs
@@ -15,7 +15,7 @@ public class DeadState : State {
 	public override void Enter (){
 		//Play dead-animation OR use a ragdoll :D
 		_controller.SetAnim("die", true);
-		Debug.Log (transform.name + ": " + _controller.CurrentState.name);
+//		Debug.Log (transform.name + ": " + _controller.CurrentState.name);
 //		transform.eulerAngles = new Vector3 (90, transform.eulerAngles.y, transform.eulerAngles.z);
 		transform.GetComponent<NavMeshAgent> ().enabled = false;
 		transform.GetComponent<BoxCollider> ().enabled = false;
--- HellDorado/Assets/Scripts/PuzzleScripts/Door.cs
+++ HellDorado/Assets/Scripts/PuzzleScripts/Door.cs
@@ -5,10 +5,19 @@ using UnityEngine;
 public class Door : MonoBehaviour {
 
 	private Animator anim;
+	private bool open;
 	public GameObject[] keys;
 
 	void Awake(){
 		anim = GetComponent<Animator> ();
+	}
+
+	public void ToggleDoor(){
+		if (open) {
+			CloseDoor ();
+		} else {
+			OpenDoor ();
+		}
 	}
 
 	public void OpenDoor(){
@@ -18,6 +27,12 @@ public class Door : MonoBehaviour {
 			}
 		}
 		anim.SetBool ("open", true);
+		open = true;
+	}
+
+	public void CloseDoor(){
+		anim.SetBool ("open", false);
+		open = false;
 	}
 
 }
