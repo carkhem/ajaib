3d29c2916b6c1bdfca2f37aed3e2347ca5883489
DESKTOP-MGSEQD0\Hannes <hanneskempelundin@gmail.com>
2018-05-08 00:28:15 +0200

    EnemyFireballShoot

---
+++ HellDorado/Assets/Scripts/EnemyFireBall.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyFireBall : MonoBehaviour {
+
+	public GameObject player;
+	public GameObject fireBallPrefab;
+	public Transform fireBallSpawn;
+
+	public float shootDelay = 5f;
+	private float originalDelay;
+	public float fireBallDamage;
+
+	// Use this for initialization
+	void Start () {
+		originalDelay = shootDelay;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.LookAt (player.transform);
+
+		shootDelay -= Time.deltaTime;
+		if (shootDelay <= 0f) {
+			shootDelay = originalDelay;
+			ShootFireBall ();
+		}
+	}
+
+
+	private void ShootFireBall(){
+		GameObject ball = Instantiate(fireBallPrefab, fireBallSpawn.position, fireBallSpawn.rotation);
+		ball.GetComponent<Fireball2> ().SetMaxDamage (fireBallDamage);
+		ball.GetComponent<Fireball2> ().lifetime = 7f;
+	}
+}
