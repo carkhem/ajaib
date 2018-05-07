480d1d6ede53505987c2d64f1c558d45f7f0d4cc
carkhem <38533272+carkhem@users.noreply.github.com>
2018-05-07 17:49:54 +0200

    Lite mer... LJUD

---
+++ HellDorado/Assets/Scripts/BoulderSound.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoulderSound : MonoBehaviour {
+
+	public AudioClip boulderSound;
+	private AudioSource source;
+
+	void Start () {
+		source = GetComponent<AudioSource> ();
+		source.PlayOneShot (boulderSound);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
