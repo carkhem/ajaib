3f76c6dd84f71645d9f8a3e3e94a40eaf3fb63d0
SeanSibthorpe <seansg369@gmail.com>
2018-05-08 02:42:39 +0200

    Pussel i lvl 2

---
+++ HellDorado/Assets/BloodPodiumEnemy.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BloodPodiumEnemy : MonoBehaviour {
+
+    public GameObject podiumCollider;
+    public GameObject host;
+    public bool ressing;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (host.GetComponent<EnemyController>().health == 0)
+            Invoke("host.GetComponent<EnemyController>().Revive()", 10);
+		//if(GetComponentInParent<PlayerStats>)
+	}
+}
---
+++ HellDorado/Assets/Scripts/ActivateBloodPodium.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActivateBloodPodium : MonoBehaviour {
+    public GameObject host;
+    private bool ressing;
+	// Use this for initialization
+	void Start () {
+        ressing = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (host.GetComponent<EnemyController>().health <= 0)
+        {
+            this.gameObject.SetActive(true);
+            if (ressing == false)
+            {
+                Invoke("host.GetComponent<EnemyController>().Revive()", 10);
+                ressing = true;
+            }
+        }
+        else
+        {
+            this.gameObject.SetActive(false);
+            ressing = false;
+        }
+		
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        other.GetComponent<BloodPodium>().filled = true;
+        //this.gameObject.SetActive(false);
+    }
+}
