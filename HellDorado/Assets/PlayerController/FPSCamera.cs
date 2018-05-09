63c643ae587c945a8fb515399b5a87769ea91c95
Aulbn <38533964+Aulbn@users.noreply.github.com>
2018-05-09 18:36:29 +0200

    kamera

--- HellDorado/Assets/PlayerController/FPSCamera.cs
+++ HellDorado/Assets/PlayerController/FPSCamera.cs
@@ -12,56 +12,32 @@ public class FPSCamera : MonoBehaviour {
 	public float Sensitivity = 5.0f;
 	public float Smoothing = 2.0f;
 	public MinMaxFloat yClamp;
-	private MinMaxFloat xClamp;
-	private MinMaxFloat currentYClamp;
-	private bool constrained;
+	private bool isStatic = false;
 
-	private float vrot;
+	float yRotate = 0.0f;
+	float xRotate = 0.0f;
 
 	private GameObject player;
 
 	void Start(){
 		player = PlayerStats.instance.gameObject;
-		currentYClamp = yClamp;
-		constrained = false;
 	}
 
 	void Update(){
-		var md = new Vector2 (Input.GetAxisRaw ("Mouse X"), Input.GetAxisRaw ("Mouse Y"));
+		if (!isStatic) {
+			yRotate += Input.GetAxis ("Mouse Y") * Sensitivity;
+			xRotate = Input.GetAxisRaw ("Mouse X") * Sensitivity + player.transform.eulerAngles.y;
+			yRotate = Mathf.Clamp (yRotate, yClamp.Min, yClamp.Max);
 
-		md = Vector2.Scale(md,new Vector2(Sensitivity * Smoothing, Sensitivity * Smoothing));
-		_smoothV.x = Mathf.Lerp (_smoothV.x, md.x, 1f / Smoothing);
-		_smoothV.y = Mathf.Lerp (_smoothV.y, md.y, 1f / Smoothing);
-		_mouseLook += _smoothV;
-
-		_mouseLook.y = Mathf.Clamp (_mouseLook.y, currentYClamp.Min, currentYClamp.Max);
-		transform.localRotation = Quaternion.AngleAxis (-_mouseLook.y, Vector3.right);
-		Vector3 _rotation = new Vector3 (0, md.x, 0) * Sensitivity;
-		player.transform.Rotate (_rotation);
-
-
-
-//		transform.Rotate (Vector3.right, md.y);
-//		transform.localRotation = Mathf.Clamp(md.y
-//		print (md.y);
-//		vrot -= Input.GetAxis ("Mouse Y") * Sensitivity;
-//		vrot = Mathf.Clamp (vrot, xClamp.Min, xClamp.Max);
-//		transform.localRotation = Quaternion.Euler (vrot, 0, 0);
+			transform.localEulerAngles = new Vector3 (-yRotate, 0, 0.0f);
+			player.transform.eulerAngles = new Vector3 (0, xRotate, 0.0f);
 
 			UpdateCursorLock ();
+		}
     }
 
-	public void SetConstraints(float minX, float maxX, float minY, float maxY){
-		currentYClamp.Min = minY;
-		currentYClamp.Max = maxY;
-		xClamp.Min = player.transform.eulerAngles.y + minX;
-		xClamp.Max = player.transform.eulerAngles.y + maxX;
-		constrained = true;
-	}
-
-	public void RemoveConstraints (){
-		currentYClamp = yClamp;
-		constrained = false;
+	public void SetStatic (bool condition){
+		
 	}
 
 	public void UpdateCursorLock()
