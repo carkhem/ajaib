e76caa04d55fb070bcfc92b6c7f7452a78271b24
Aulbn <38533964+Aulbn@users.noreply.github.com>
2018-05-09 23:41:17 +0200

    Level1 fixes

--- HellDorado/Assets/Scripts/AnimatedKey.cs
+++ HellDorado/Assets/Scripts/AnimatedKey.cs
@@ -15,7 +15,7 @@ public class AnimatedKey : MonoBehaviour {
 			return;
 
 		if (director.time > 0 && director.time < 6.52f) {
-//			PlayerStats.instance.transform.GetComponent<PlayerController> ().enabled = false;
+			PlayerStats.instance.transform.GetComponent<PlayerController> ().enabled = false;
 			transform.localPosition = new Vector3 (0.0016f, -0.0017f, 0);
 		} else  if (director.time >= 6.52 && director.time < 7) {
 			if (!dropped) {
