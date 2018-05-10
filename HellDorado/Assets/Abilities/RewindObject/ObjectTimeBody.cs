77d9767c1dcbe13e1a94fed5e9596f0ebeab1be7
hankempe <38531462+hankempe@users.noreply.github.com>
2018-05-10 21:18:08 +0200

    spöke på objekt rewind- inte klart än suka blyat

--- HellDorado/Assets/Abilities/RewindObject/ObjectTimeBody.cs
+++ HellDorado/Assets/Abilities/RewindObject/ObjectTimeBody.cs
@@ -7,17 +7,11 @@ public class ObjectTimeBody : MonoBehaviour {
 	public bool isRewinding = false;
 	public float recordTime = 5f;
 
-	List<PointInTime> pointsInTime;
+	private List<PointInTime> pointsInTime;
 	private Rigidbody rb;
 	private PlayerController _controller;
 	private AbilitySounds abilitySounds;
 
-	//Lämna tills vidare
-	//	public Transform shadowObject;
-	//	public Transform clone;
-	//	public bool shadowObjectCreated = false;
-	//	public bool deactivateObject = false;
-	//	public Material shadowMaterial;
 
 	void Start () {
 		pointsInTime = new List<PointInTime>();
@@ -62,19 +56,13 @@ public class ObjectTimeBody : MonoBehaviour {
 
 		if(rb.velocity.sqrMagnitude > 0.01f)
 			pointsInTime.Insert(0, new PointInTime(transform.position, transform.rotation));
-
-
-		//Lämna tills vidare
-		//		if (clone != null && pointsInTime.Count > 0) {
-		//			clone.position = pointsInTime [pointsInTime.Count - 1].position;
-		//			clone.rotation = pointsInTime [pointsInTime.Count - 1].rotation;
-		//		}
 	}
 
 	public void StartRewind ()
 	{
 		isRewinding = true;
-		rb.isKinematic = true;
+        if(rb != null)
+		    rb.isKinematic = true;
 //		abilitySounds.PlayAbilitySound ("RewindObject");
 
 	}
@@ -82,51 +70,17 @@ public class ObjectTimeBody : MonoBehaviour {
 	public void StopRewind ()
 	{
 		isRewinding = false;
-		rb.isKinematic = false;
+        if(rb != null)
+		    rb.isKinematic = false;
 //		abilitySounds.StopPlayingAudio();
 	}
 
+    public List<PointInTime> GetPointsInTime() {
+        return pointsInTime;
+    }
 
-	//lämna tills vidare
-	//	public void CreateShadowObject(){
-	//
-	//		if (shadowObject != null) {
-	//			clone = Instantiate (shadowObject, transform.position, Quaternion.identity);
-	//			clone.gameObject.GetComponent<MeshRenderer> ().material = shadowMaterial;
-	//			Destroy (clone.gameObject.GetComponent<Rigidbody> ());
-	//			Destroy(clone.gameObject.GetComponent<BoxCollider> ());
-	//			Destroy (clone.gameObject.GetComponent<RewindObject> ());
-	//			clone.gameObject.layer = 0;
-	//			clone.gameObject.SetActive (false);
-	//			if (pointsInTime.Count > 0) {
-	//				clone.position = pointsInTime [pointsInTime.Count - 1].position;
-	//				clone.rotation = pointsInTime [pointsInTime.Count - 1].rotation;
-	//			}
-	//
-	//		}
-	//	}
-	//
-	//	public void ActivateShadowObject(){
-	//		if (clone != null && shadowObjectCreated == true) {
-	//			if (rb.velocity.sqrMagnitude < 0.01f)
-	//				if(pointsInTime.Count > 0 && pointsInTime[0].position != pointsInTime[pointsInTime.Count-1].position)
-	//					clone.gameObject.SetActive (true);
-	//		} else {
-	//			shadowObjectCreated = true;
-	//			CreateShadowObject ();
-	//		}
-	//	}
-	//
-	//	public void DestroyObject(){
-	//		if (clone != null) {
-	//			clone.gameObject.SetActive (false);
-	//			Debug.Log ("WHYYYYY");
-	//			deactivateObject = false;
-	//		}	
-	//	}
-	//
-	//	public void DeactivateObject(){
-	//
-	//		deactivateObject = true;
-	//	}
+    public void SetPointsInTime(List<PointInTime> list) {
+        pointsInTime = list;
+    }
+
 }
--- HellDorado/Assets/Abilities/RewindObject/RewindObject.cs
+++ HellDorado/Assets/Abilities/RewindObject/RewindObject.cs
@@ -7,8 +7,13 @@ public class RewindObject : MonoBehaviour {
 	private GameObject activeGameobject;
 	public GameObject interactionIcon;
 	private GameObject currentIcon;
+    private GameObject clone;
+    private GameObject objectToCloneFrom;
 
-	public void UseRewindObject(){
+    private List<PointInTime> clonePointInTime;
+    private List<PointInTime> originalPointInTime;
+
+    public void UseRewindObject(){
 		Ray ray = Camera.main.ScreenPointToRay(new Vector3 (Screen.width / 2, Screen.height / 2, 0));
 		RaycastHit hit;
 
@@ -23,7 +28,22 @@ public class RewindObject : MonoBehaviour {
 		RaycastHit hit;
 
 		if ((Physics.Raycast (ray, out hit, 50) && (hit.collider.gameObject.tag == "Interactable")) && hit.collider.GetComponent<ObjectTimeBody> () != null) {
-			if (!Input.GetButton ("Fire2")){
+            if (!Input.GetButton("Fire2"))
+            {
+                if (objectToCloneFrom == null)
+                {
+                    objectToCloneFrom = hit.collider.gameObject;
+                    clone = GameObject.Instantiate(objectToCloneFrom, objectToCloneFrom.transform.position, Quaternion.identity);
+                    clone.GetComponent<BoxCollider>().enabled = false;
+                    Destroy(clone.GetComponent<Rigidbody>());
+                    clonePointInTime = objectToCloneFrom.GetComponent<ObjectTimeBody>().GetPointsInTime();
+                    Destroy(clone.GetComponent<ObjectTimeBody>());
+                    clone.GetComponent<ObjectTimeBody>().SetPointsInTime(clonePointInTime);
+                    clone.transform.position = clonePointInTime[clonePointInTime.Count - 1].position;
+
+                    //clone.GetComponent<ObjectTimeBody>().StartRewind();
+                }
+               
 				if (currentIcon == null) {
 				currentIcon = GameObject.Instantiate (interactionIcon, new Vector3 (hit.transform.position.x, hit.transform.position.y + 1, hit.transform.position.z), Quaternion.LookRotation (Camera.main.transform.position - hit.transform.position));
 				} else {
@@ -32,6 +52,8 @@ public class RewindObject : MonoBehaviour {
 			}
 		} else {
 			Destroy (currentIcon);
+            Destroy(clone);
+            objectToCloneFrom = null;
 		}
 	}
 
@@ -39,7 +61,6 @@ public class RewindObject : MonoBehaviour {
 	public GameObject HitInfo(){
 		return activeGameobject;
 	}
-
 
 
 }
