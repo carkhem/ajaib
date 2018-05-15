9d0f803415695403721a378e5095dbd2c0d98458
Aulbn <38533964+Aulbn@users.noreply.github.com>
2018-05-15 14:18:31 +0200

    eldfix

--- HellDorado/Assets/Scripts/PuzzleScripts/GateTorchTimer.cs
+++ HellDorado/Assets/Scripts/PuzzleScripts/GateTorchTimer.cs
@@ -10,6 +10,7 @@ public class GateTorchTimer : MonoBehaviour {
 	public float timer = 3f;
 	private float originalTime;
 	private float lightIntensity;
+	public float fireHight = 2f;
 
 	void Start(){
 		lightIntensity = fireLight.intensity;
@@ -36,12 +37,20 @@ public class GateTorchTimer : MonoBehaviour {
 	public void Light(){
 		lit = true;
 		fire.Play ();
+		fire.startLifetime = fireHight;
+		foreach (ParticleSystem system in fire.transform.GetComponentsInChildren<ParticleSystem>()) {
+			system.startLifetime = fireHight;
+		}
 		fireLight.intensity = lightIntensity;
 	}
 
 	public void PutOut(){
 		lit = false;
 		fire.Stop ();
+		fire.startLifetime = 1f;
+		foreach (ParticleSystem system in fire.transform.GetComponentsInChildren<ParticleSystem>()) {
+			system.startLifetime = 1f;
+		}
 		fireLight.intensity = 0;
 	}
 
