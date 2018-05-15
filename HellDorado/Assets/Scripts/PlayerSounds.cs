86477f9b6e017add734c44a1dd1cb0dba23772ea
carkhem <38533272+carkhem@users.noreply.github.com>
2018-05-15 21:30:33 +0200

    Dynamic foley
    - Raycast som kollar vilket underlag det är
    - Spelar upp olika ljud beroende på vilken tag det är "Blood" och "Wood". Har den ingen tag så spelas "default" ljud

--- HellDorado/Assets/Scripts/PlayerSounds.cs
+++ HellDorado/Assets/Scripts/PlayerSounds.cs
@@ -2,26 +2,32 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerSounds : MonoBehaviour
-{
-
-    public AudioSource[] sources;
+public class PlayerSounds : MonoBehaviour {
 
     public AudioClip swordSwing;
     public AudioClip deathSound;
     public AudioClip dashSound;
     public AudioClip[] takingDamageSounds;
     public AudioClip[] jumpSounds;
-    public AudioClip[] walkingSounds;
+    public AudioClip[] defaultWalkingSounds;
+    public AudioClip[] bloodWalkingSounds;
+    public AudioClip[] woodWalkingSounds;
+
     public AudioClip keySound;
     public AudioClip landingSound;
 
+    private AudioSource[] sources; // 0 = death & swordSwing | 1 = dash | 2 = walking | 3 = takingDamage | 4 = jump
+    private string surface;
     private int clipIndex;
-    private Transform player;
 
     void Start()
     {
         sources = GetComponents<AudioSource>();
+    }
+
+    void Update()
+    {
+        surface = GetComponent<SurfaceIdentifier>().GetSurfaceType();
     }
 
     public void PlayDeathSound()
@@ -31,6 +37,7 @@ public class PlayerSounds : MonoBehaviour
 
     public void PlaySwordSwing()
     {
+        sources[2].Stop();
         sources[0].PlayOneShot(swordSwing);
     }
 
@@ -42,7 +49,7 @@ public class PlayerSounds : MonoBehaviour
     public void TakingDamageSound()
     {
         if (!sources[3].isPlaying)
-            clipIndex = Random.Range(0, takingDamageSounds.Length);
+            clipIndex = Random.Range(1, takingDamageSounds.Length);
         AudioClip clip = takingDamageSounds[clipIndex];
         sources[3].PlayOneShot(clip);
     }
@@ -60,30 +67,75 @@ public class PlayerSounds : MonoBehaviour
         sources[3].PlayOneShot(keySound);
     }
 
-    public void PlayWalkSound()
-    {
+    public void PlayWalkSound() {
         //Kallas i Headbobber
         if (transform.GetComponent<CharacterController>().isGrounded)
         {
-            clipIndex = Random.Range(1, walkingSounds.Length);
-            AudioClip clip = walkingSounds[clipIndex];
-            if (!sources[2].isPlaying)
+
+            if (gameObject.GetComponent<PlayerStats>().sneaking || sources[0].isPlaying)
             {
-                if (gameObject.GetComponent<PlayerStats>().sneaking)
-                    return;
-                sources[2].PlayOneShot(clip);
-                walkingSounds[clipIndex] = walkingSounds[0];
-                walkingSounds[0] = clip;
+                return;
+            }
+            switch (surface)
+            {
+                case "Wood":
+                    PlayWoodWalkSound();
+                    break;
+                case "Blood":
+                    PlayBloodWalkSound();
+                    break;
+                case "Default":
+                    PlayDefaultWalkSound();
+                    break;
             }
         }
     }
-    public void StopPlayWalkingSound()
+    
+        public void StopPlayWalkingSound()
     {
         sources[2].Stop();
     }
 
     public void PlayLandingSound()
     {
-        sources[1].PlayOneShot(landingSound);
+        if(!gameObject.GetComponent<PlayerStats>().sneaking) { 
+            sources[1].PlayOneShot(landingSound);
+        }
+    }
+
+    public void PlayDefaultWalkSound()
+    {
+        clipIndex = Random.Range(1, defaultWalkingSounds.Length);
+        AudioClip clip = defaultWalkingSounds[clipIndex];
+        if (!sources[2].isPlaying)
+        {
+            sources[2].PlayOneShot(clip);
+            defaultWalkingSounds[clipIndex] = defaultWalkingSounds[0];
+            defaultWalkingSounds[0] = clip;
+        }
+    }
+
+    public void PlayWoodWalkSound()
+    {
+        clipIndex = Random.Range(1, woodWalkingSounds.Length);
+        AudioClip clip = woodWalkingSounds[clipIndex];
+        if (!sources[2].isPlaying)
+        {
+            sources[2].PlayOneShot(clip);
+            woodWalkingSounds[clipIndex] = woodWalkingSounds[0];
+            woodWalkingSounds[0] = clip;
+        }
+    }
+
+    public void PlayBloodWalkSound()
+    {
+        clipIndex = Random.Range(1, bloodWalkingSounds.Length);
+        AudioClip clip = bloodWalkingSounds[clipIndex];
+        if (!sources[2].isPlaying)
+        {
+            sources[2].PlayOneShot(clip);
+            bloodWalkingSounds[clipIndex] = bloodWalkingSounds[0];
+            bloodWalkingSounds[0] = clip;
+        }
     }
 }
---
+++ HellDorado/Assets/SurfaceIdentifier.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SurfaceIdentifier : MonoBehaviour {
+
+    private Vector3 direction;
+    public float distance;
+    private RaycastHit hit;
+    public string surfaceType;
+
+	// Use this for initialization
+	void Start () {
+        direction =  new Vector3(0, -1, 0);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Debug.DrawRay(transform.position, direction * distance, Color.green);
+        if((Physics.Raycast(transform.position, direction, out hit, distance))) {
+
+            if (hit.collider.gameObject.tag == "Blood") {
+                surfaceType = "Blood";
+                return;
+            }
+
+            if (hit.collider.gameObject.tag == "Wood") {
+                surfaceType = "Wood";
+                return;
+
+            }else{
+                surfaceType = "Default";
+            }
+        }
+	}
+
+    public string GetSurfaceType()
+    {
+        return surfaceType;
+    }
+}
