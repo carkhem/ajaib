1711b9f0f0c1ddd0c8abcd52b4492a3472e208a8
Albin <38533964+Aulbn@users.noreply.github.com>
2018-05-17 12:20:13 +0200

    Level 1 - Arms

--- HellDorado/Assets/PlayerController/PlayerController.cs
+++ HellDorado/Assets/PlayerController/PlayerController.cs
@@ -32,8 +32,7 @@ public class PlayerController : Controller{
 		movementSpeed = maxSpeed;
 		if (startWithSword) {
             EquipSword ();
-            CanvasManager.instance.abilityBar.GetComponent<AbilityBar>().ChangeWeapon(1);
-            
+            CanvasManager.instance.abilityBar.GetComponent<AbilityBar>().ChangeWeapon(1);   
         } else {
 			lameRHand.SetActive (true);
 			rArmAnim.gameObject.SetActive (false);
