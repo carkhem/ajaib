326f91e7ddd07ef106cb31e0e28d772e50dbde16
Aulbn <38533964+Aulbn@users.noreply.github.com>
2018-05-21 21:15:44 +0200

    Ability up

---
+++ HellDorado/Assets/AbilityUp.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AbilityUp : MonoBehaviour {
+
+	public void SetAbility(int ammount){
+		GameManager.instance.SetAbilityCount (ammount);
+	}
+}
