af104e40a063b67d980ea74888e90fe1c7023397
Albin <38533964+Aulbn@users.noreply.github.com>
2018-05-22 10:01:35 +0200

    BloodKill

---
+++ HellDorado/Assets/GameOverScript.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameOverScript : MonoBehaviour {
+	public void GameOver(){
+		GameManager.instance.GameOver ();
+	}
+}
